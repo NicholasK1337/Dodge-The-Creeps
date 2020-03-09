e703fd3b75e1e33fcfbd3daa1b15766aaad6df94
21kownackinicholas <21kownackinicholas@MSDBOE.ORG>
2020-03-09 10:35:58 -0400

    first commit

---
+++ Mob.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+var mob_types = ["Walk", "Swim", "Fly"]
+
+func _ready():
+    $AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+func _on_Visibility_screen_exited():
+	queue_free()
+	
+
+	
